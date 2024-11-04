0959ba12f3258d06bce5d7148e4802583430751a
bekirtechpro <bekirtechproeducation@gmail.com>
2024-11-04 23:21:13 +0300

    task-3-solution files added

---
+++ Task-3-Solution/terraform/variable.tf
@@ -0,0 +1,39 @@
+variable "myami" {
+  type = map(string)
+  default = {
+    dev = "ami-04e5276ebb8451442"
+    prod  = "ami-04e5276ebb8451442"
+    test   = "ami-08d4ac5b634553e16"
+    staging   = "ami-08d4ac5b634553e16"
+  }
+  description = "in order of Amazon Linux 2023 ami, Red Hat Enterprise Linux 8 ami and Ubuntu Server 20.04 LTS"
+}
+
+
+variable "ports" {
+  type = map(list(number))
+  default = {
+    default = [80, 443, 22]
+    dev= [80, 443, 22, 5432, 3000, 5000, 3306]
+    test= [80, 443, 22, 8080]
+    prod= [22, 80, 443, 8080, 9000, 3000]
+    staging= [22, 80, 443, 8080, 9000]
+  }
+}
+
+
+variable "region" {
+  default = "us-east-1"  
+}
+
+
+variable "ec2_type" {
+  default = "t2.micro"
+}
+variable "ec2_key" {
+  default = "techpro" # change here
+}
+
+variable "num_of_instance" {
+  default = 1
+}
